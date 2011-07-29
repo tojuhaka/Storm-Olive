9eb10c28b8491a00bf73959fe6008747ffc474c2
Toni Haka-Risku <tojuhaka@gmail.com>
2011-07-29 23:29:58 +0300

    named-routes done

--- spec/requests/layout_links_spec.rb
+++ spec/requests/layout_links_spec.rb
@@ -11,4 +11,9 @@ describe "LayoutLinks" do
     get '/blog'
     response.should have_selector('title', :content => "Blog")
   end
+
+  it "should have a signup page at '/signup'" do
+    get '/signup'
+    response.should have_selector('title', :content => "Sign up")
+  end
 end
