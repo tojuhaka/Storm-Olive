30e865e4d66daa5fb55d4c4be9f94ba0c1c3dfed
Toni Haka-Risku <tojuhaka@gmail.com>
2011-07-29 09:28:20 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StormOlive::Application.config.secret_token = 'db4d03f481ad6bad727baed3d5aa767285a47440199588a2fbd2fd36f584a4b65cf54dcc29b135c156a8b1561d6c209ca3feb60ac1da4163accf71a1bc0755b5'
